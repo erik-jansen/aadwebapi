8014a67f82939c6dc18aa765bfef2d07d7a5e548
Erik Jansen <ej@live.nl>
2017-05-19 15:13:35 +1000

    Init

---
+++ StudentApi/App_Start/Startup.Auth.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+
+namespace StudentApi
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
+                    },
+                });
+        }
+    }
+}
---
+++ StudentApi/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace StudentApi.Controllers
+{
+    [Authorize]
+    public class StudentController : ApiController
+    {
+        public string Get()
+        {
+            return "This is a fake student API";
+        }
+    }
+}
